8199e9c8cf382f679f92632d298fd67912b5adcc
Hugh O'Reilly <G00364715@GMIT.IE>
2018-04-04 10:37:06 +0100

    quotes fixed

--- IrisData.py
+++ IrisData.py
@@ -3,7 +3,7 @@
 with open("data/iris.csv") as f: #Opens Iris data set csv file in data folder
     for line in f:# loops through each line
         line = line.replace(',', '  ') #replaces comma with space, code from Mohamed Noor
-        line = line.rstrip() #Removes nextline code on end,  code from Mohamed Noor
+        line = line.rstrip() #Removes nextline code on end, code from Mohamed Noor
         print(line.split(',')[:]) #Splits and Prints 
         #each line as a list, colon separates each item in
         # columns
