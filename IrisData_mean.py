4727e2980f81f0df6d6a81d98ea3e0abe81d1117
Hugh O'Reilly <G00364715@GMIT.IE>
2018-04-10 22:38:24 +0100

    More work on IrisData_mean.py to get plots working, creation of iris_data_np_v2 to split up data set

--- IrisData_mean.py
+++ IrisData_mean.py
@@ -1,31 +1,30 @@
-import math
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-dataset = pd.read_csv("data/iris.csv") #import dataset as panda
-dataset_np = np.array(dataset) #convert to numpy array
-x = dataset_np [:3] #select first 4 columns
-def mean(x):
-    return sum ((x)/len(x)) #write function to define mean
 
+iris = pd.read_csv("data/iris.csv") #import dataset as panda
+iris_np = np.array(iris) #convert to numpy array
 
- 
-print ("Average of column 1 (Sepal Length) for all species is:", float("{0:.3f}".format(mean(dataset_np[:, 0]))))
-print ("Average of column 2 (Sepal Width) for all species is:\n", float("{0:.3f}".format(mean(dataset_np[:, 1]))))
-print ("Average of column 3 (Petal Length) for all species is:\n", float("{0:.3f}".format(mean(dataset_np[:, 2]))))
-print ("Average of column 4 (Petal Width) for all species is:\n", float("{0:.3f}".format(mean(dataset_np[:, 3]))))
+print ("Average of column 1 (Sepal Length) for all species is:", float("{0:.3f}".format(np.mean(iris_np[:, 0]))))
+print ("Average of column 2 (Sepal Width) for all species is:\n", float("{0:.3f}".format(np.mean(iris_np[:, 1]))))
+print ("Average of column 3 (Petal Length) for all species is:\n", float("{0:.3f}".format(np.mean(iris_np[:, 2]))))
+print ("Average of column 4 (Petal Width) for all species is:\n", float("{0:.3f}".format(np.mean(iris_np[:, 3]))))
 
-print ("Average of column 1 (Sepal Length) for Iris Setosa:\n",float("{0:.3f}".format(mean(dataset_np[51:, 0])))) 
-print ("Average of column 2 (Sepal Width) for Iris Setosa:\n", float("{0:.3f}".format(mean(dataset_np[51:, 1]))))
-print ("Average of column 3 (Petal Length) for Iris Setosa:\n", float("{0:.3f}".format(mean(dataset_np[51:, 2]))))
-print ("Average of column 4 (Petal Width) for Iris Setosa:\n", float("{0:.3f}".format(mean(dataset_np[51:, 3]))))
+print ("Average of column 1 (Sepal Length) for Iris Setosa:\n",float("{0:.3f}".format(np.mean(iris_np[51:, 0])))) 
+print ("Average of column 2 (Sepal Width) for Iris Setosa:\n", float("{0:.3f}".format(np.mean(iris_np[51:, 1]))))
+print ("Average of column 3 (Petal Length) for Iris Setosa:\n", float("{0:.3f}".format(np.mean(iris_np[51:, 2]))))
+print ("Average of column 4 (Petal Width) for Iris Setosa:\n", float("{0:.3f}".format(np.mean(iris_np[51:, 3]))))
 
-print ("Average of column 1 (Sepal Length) for Iris Versicolor:\n",float("{0:.3f}".format(mean(dataset_np[51:101, 0])))) 
-print ("Average of column 2 (Sepal Width) for Iris Versicolor:\n", float("{0:.3f}".format(mean(dataset_np[51:101, 1]))))
-print ("Average of column 3 (Petal Length) for Iris Versicolor:\n",float("{0:.3f}".format(mean(dataset_np[51:101, 2])))) 
-print ("Average of column 4 (Petal Width) for Iris Versicolor:\n", float("{0:.3f}".format(mean(dataset_np[51:101, 3]))))
+print ("Average of column 1 (Sepal Length) for Iris Versicolor:\n",float("{0:.3f}".format(np.mean(iris_np[51:101, 0])))) 
+print ("Average of column 2 (Sepal Width) for Iris Versicolor:\n", float("{0:.3f}".format(np.mean(iris_np[51:101, 1]))))
+print ("Average of column 3 (Petal Length) for Iris Versicolor:\n",float("{0:.3f}".format(np.mean(iris_np[51:101, 2])))) 
+print ("Average of column 4 (Petal Width) for Iris Versicolor:\n", float("{0:.3f}".format(np.mean(iris_np[51:101, 3]))))
 
-print ("Average of column 1 (Sepal Length) for Iris Virginica:\n", (float("{0:.3f}".format(mean(dataset_np[101:, 0]))))) 
-print ("Average of column 2 (Sepal Width) for Iris Virginica:\n", float("{0:.3f}".format(mean(dataset_np[101:, 1]))))
-print ("Average of column 3 (Petal Length) for Iris Virginica:\n", float("{0:.3f}".format(mean(dataset_np[101:, 2]))))
-print ("Average of column 4 (Petal Width) for Iris Virginica:\n",float("{0:.3f}".format(mean(dataset_np[101:, 3])))) 
+print ("Average of column 1 (Sepal Length) for Iris Virginica:\n", (float("{0:.3f}".format(np.mean(iris_np[101:, 0]))))) 
+print ("Average of column 2 (Sepal Width) for Iris Virginica:\n", float("{0:.3f}".format(np.mean(iris_np[101:, 1]))))
+print ("Average of column 3 (Petal Length) for Iris Virginica:\n", float("{0:.3f}".format(np.mean(iris_np[101:, 2]))))
+print ("Average of column 4 (Petal Width) for Iris Virginica:\n",float("{0:.3f}".format(np.mean(iris_np[101:, 3])))) 
+import matplotlib.pyplot as pl
+
+pl.hist(iris_np[51:, 0])
+pl.show()
+
---
+++ Iris_Data_np_v2.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import numpy as np
+
+iris = pd.read_csv("data/iris.csv") #import dataset as panda
+iris_np = np.array(iris) #convert to numpy array
+
+np.vsplit(iris_np, 49) #split iris data set by row 49 (iris Setosa subset)
+
+iris setosa = (np.vsplit(iris_np, 49))
